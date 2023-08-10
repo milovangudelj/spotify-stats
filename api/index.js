7bb94166ef01991ede979d91227e788598e5f3b7
milovangudelj <milovan.gudelj@gmail.com>
2023-08-11 00:53:34 +0200

    Setu changes and config

--- api/index.js
+++ api/index.js
@@ -1,15 +1,15 @@
 import "dotenv/config";
 import express from "express";
 
-import getAlbum from "../api/getAlbum.js";
+import getAlbum from "../src/getAlbum.js";
 
 const app = express();
 const port = process.env.SERVER_PORT || 3000;
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`App listening on port ${port}`);
 });
 
-app.get("/", getAlbum);
+app.get("/api", getAlbum);
 
 export default app;
